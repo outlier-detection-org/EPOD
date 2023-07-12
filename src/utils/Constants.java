ef09a4fd59c7863db7b621dcdb87d063e41e4bc8
Nancyzxy <11912039@mail.sustech.edu.cn>
2023-07-12 10:51:31 +0800

    modify print pattern to better debug

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static int dn = 2;
     public static int nW = 1;
     public static String methodToGenerateFingerprint = "NETS"; //"NETS" "MCOD" "NETS_CENTRALIZE" "" "NETS_P2P" "MCOD_P2P"
-    public static String dataset = "GAU"; //"FC"(¡Á) "TAO" "GAS" "STK" "GAU" "EM" "HPC"
+    public static String dataset = "HPC"; //"FC"(¡Á) "TAO" "GAS" "STK" "GAU" "EM" "HPC"
 
     //calculated automatically
 
